f1920eb533ec204d8c2085e57198e65cc620b8dd
garethrichings33 <garethrichings@hotmail.com>
2024-02-29 19:55:44 +0000

    Fixed broken test template.

--- src/test/java/com/github/garethrichings33/ContactsBookTest.java
+++ src/test/java/com/github/garethrichings33/ContactsBookTest.java
@@ -1,38 +1,5 @@
 package com.github.garethrichings33;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+public class ContactsBookTest{
 
-/**
- * Unit test for simple ContactsBook.
- */
-public class ContactsBookTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public ContactsBookTest(String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( ContactsBookTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
